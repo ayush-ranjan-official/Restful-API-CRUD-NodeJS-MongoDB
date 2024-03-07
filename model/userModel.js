b9d399531a2946ff7c861a4eccd44902431461dd
Ayush Ranjan <93761136+ayush-ranjan-official@users.noreply.github.com>
2024-03-07 22:40:12 +0530

    Update userModel.js

--- model/userModel.js
+++ model/userModel.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
+    _id: {
+        type: String,
+        required: true,
+    },
     SurvivalTime: {
         type: Number,
         required: [true, "Please enter a SurvivalTime"]
@@ -37,3 +41,13 @@ const userSchema = new mongoose.Schema({
 })
 
 export default mongoose.model("XPSystem",userSchema)
+
+/*
+The versionKey is a property set on each document when first created by Mongoose. 
+This keys value contains the internal revision of the document. 
+The name of this document property is configurable. The default is __v = 0.
+
+If this conflicts with your application you can configure as such:
+
+new Schema({..}, { versionKey: '_somethingElse' }) or versionKey: false 
+*/
