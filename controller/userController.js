e0c9331fb8e89f1dca20888677d628c10c146302
Ayush Ranjan <93761136+ayush-ranjan-official@users.noreply.github.com>
2024-03-07 22:39:40 +0530

    Update userController.js

--- controller/userController.js
+++ controller/userController.js
@@ -3,8 +3,8 @@ import User from "../model/userModel.js"
 export const create = async(req, res)=>{
     try {
         const userData = new User( req.body);
-        const {email} = userData;
-        const userExist = await User.findOne({email})
+        const {_id} = userData;
+        const userExist = await User.findOne({_id})
         if (userExist){
             return res.status(400).json({message : "User already exist."})
         }
